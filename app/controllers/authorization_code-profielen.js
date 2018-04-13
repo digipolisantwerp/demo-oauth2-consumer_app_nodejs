ea66bcae5363ab4b398288deef152faa2d510d1c
Peter Brion <peter.brion@digipolis.be>
2018-04-13 11:42:58 +0200

    Formatting & ESLint fixes

--- app/controllers/authorization_code-profielen.js
+++ app/controllers/authorization_code-profielen.js
@@ -2,15 +2,12 @@
 
 var request = require('request');
 var querystring = require('querystring');
-var yamlConfig = require('node-yaml-config');
 var OAuth2 = require('oauth').OAuth2;
-
 
 function getConfig() {
   var config = require(global.__base + '/config/services.conf.js');
   return JSON.parse(JSON.stringify(config));
 }
-
 
 function createAuthorizeUrl(type) {
   var envConfig = getConfig();
@@ -20,19 +17,15 @@ function createAuthorizeUrl(type) {
 
   configOauth.lng = 'nl';
   configOauth.state = '32042809';
-  delete configOauth['client_secret'];
+  delete configOauth.client_secret;
   url += querystring.stringify(configOauth);
 
   return url;
 }
 
-
 function index(req, res) {
-
-  res.render('index.ejs', {urlAProfiel: createAuthorizeUrl('aprofiel'), urlMProfiel: createAuthorizeUrl('mprofiel')});
-
+  res.render('index.ejs', { urlAProfiel: createAuthorizeUrl('aprofiel'), urlMProfiel: createAuthorizeUrl('mprofiel') });
 }
-
 
 function callbackAprofiel(req, res) {
   var envConfig = getConfig();
@@ -40,36 +33,35 @@ function callbackAprofiel(req, res) {
   var configApi = envConfig.aprofiel.uri;
 
   var oauth2 = new OAuth2(configOauth.client_id,
-      configOauth.client_secret,
-      configApi.scheme + '://' + configApi.domain,
-      null,
-      configApi.path + '/oauth2/token',
-      null);
-    
-  oauth2.getOAuthAccessToken(req.query.code, {'grant_type': 'authorization_code'}
-      , function handleTokenResponse(err, token) {
-        console.log(token);
-        if (err) {
-          res.json({
-            error: err
-          });
-        } else {
-          request({
-            url: configApi.scheme + '://' + configApi.domain + configApi.path + '/v1/me',
-            'auth': {
-              'bearer': token
-            }
+    configOauth.client_secret,
+    configApi.scheme + '://' + configApi.domain,
+    null,
+    configApi.path + '/oauth2/token',
+    null);
 
-          }, function handleApiCall(error, response, body) {
-            if (error) {
-              return res.send(error);
-            }
-            return res.send(body);
-          });
+  oauth2.getOAuthAccessToken(req.query.code, { 'grant_type': 'authorization_code' }
+    , function handleTokenResponse(err, token) {
+      console.log(token);
+      if (err) {
+        res.json({
+          error: err
+        });
+      } else {
+        request({
+          url: configApi.scheme + '://' + configApi.domain + configApi.path + '/v1/me',
+          'auth': {
+            'bearer': token
+          }
 
-        }
-      });
+        }, function handleApiCall(error, response, body) {
+          if (error) {
+            return res.send(error);
+          }
+          return res.send(body);
+        });
 
+      }
+    });
 }
 
 function callbackMprofiel(req, res) {
@@ -78,36 +70,37 @@ function callbackMprofiel(req, res) {
   var configApi = envConfig.mprofiel.uri;
 
   var oauth2 = new OAuth2(configOauth.client_id,
-      configOauth.client_secret,
-      configApi.scheme + '://' + configApi.domain,
-      null,
-      configApi.path + '/oauth2/token',
-      null);
+    configOauth.client_secret,
+    configApi.scheme + '://' + configApi.domain,
+    null,
+    configApi.path + '/oauth2/token',
+    null);
 
-  oauth2.getOAuthAccessToken(req.query.code, {'grant_type': 'authorization_code'}
-      , function handleTokenResponse(err, token) {
-        console.log(token);
-        if (err) {
-          res.json({
-            error: err
-          });
-        } else {
-          request({
-            url: configApi.scheme + '://' + configApi.domain + configApi.path + '/v1/me',
-            'auth': {
-              'bearer': token
-            }
+  oauth2.getOAuthAccessToken(
+    req.query.code,
+    { 'grant_type': 'authorization_code' },
+    function handleTokenResponse(err, token) {
+      console.log(token);
+      if (err) {
+        res.json({
+          error: err
+        });
+      } else {
+        request({
+          url: configApi.scheme + '://' + configApi.domain + configApi.path + '/v1/me',
+          'auth': {
+            'bearer': token
+          }
+        }, function handleApiCall(error, response, body) {
+          if (error) {
+            return res.send(error);
+          }
+          return res.send(body);
+        });
 
-          }, function handleApiCall(error, response, body) {
-            if (error) {
-              return res.send(error);
-            }
-            return res.send(body);
-          });
-
-        }
-      });
-
+      }
+    }
+  );
 }
 
 module.exports = {
