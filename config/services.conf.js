64abaa0f8f577c47cd8061f97096c785df3cf5fc
Olivier Van den Mooter <olivier.vandenmooter@icapps.com>
2018-06-21 12:52:04 +0200

    Add Eid support to demo app

--- config/services.conf.js
+++ config/services.conf.js
@@ -4,15 +4,31 @@ module.exports = {
   consent: {
     uri: {
       scheme: process.env.SERVICE_CONSENT_URI_SCHEME || 'https',
-      domain: process.env.SERVICE_CONSENT_URI_DOMAIN || 'api-oauth2-o.antwerpen.be',
+      domain: process.env.SERVICE_CONSENT_URI_DOMAIN || 'localhost:4000',
       path: '/v1/authorize?'
     }
+  },
+  fasdatastore: {
+    uri: {
+      scheme: process.env.SERVICE_fasdatastore_URI_SCHEME || 'https',
+      domain: process.env.SERVICE_fasdatastore_URI_DOMAIN || 'api-gw-o.antwerpen.be',
+      path: '/acpaas/fasdatastore/v1',
+      api_key: process.env.SERVICE_fasdatastore_API_KEY || 'YOUR_API_KEY',
+    },
+    auth: {
+      response_type: 'code',
+      service: process.env.SERVICE_fasdatastore_AUTH_SERVICE || 'acpaas.fasdatastore.v1',
+      client_id: process.env.SERVICE_fasdatastore_AUTH_CLIENT_ID || 'YOUR_CLIENT_ID',
+      client_secret: process.env.SERVICE_fasdatastore_AUTH_CLIENT_SECRET || 'YOUR_CLIENT_SECRET',
+      scope: 'name',
+      redirect_uri: process.env.SERVICE_fasdatastore_AUTH_REDIRECT_URI || 'YOUR_REDIRECT_URI',
+    },
   },
   aprofiel: {
     uri: {
       scheme: process.env.SERVICE_APROFIEL_URI_SCHEME || 'https',
       domain: process.env.SERVICE_APROFIEL_URI_DOMAIN || 'api-gw-o.antwerpen.be',
-      path: '/astad/aprofiel/v1'
+        path: '/astad/aprofiel/v1/v1'
     },
     auth: {
       response_type: 'code',
@@ -27,7 +43,7 @@ module.exports = {
     uri: {
       scheme: process.env.SERVICE_MPROFIEL_URI_SCHEME || 'https',
       domain: process.env.SERVICE_MPROFIEL_URI_DOMAIN || 'api-gw-o.antwerpen.be',
-      path: '/astad/mprofiel/v1'
+        path: '/astad/mprofiel/v1/v1'
     },
     auth: {
       response_type: 'code',
