9515b95cf94872dd7826ead797589e96b84ee67c
Olivier Van den Mooter <olivier@vademo.be>
2019-08-21 14:07:41 +0200

    Rename aprofiel to  iam-aprofiel-userpass

--- config/services.conf.js
+++ config/services.conf.js
@@ -60,7 +60,7 @@ module.exports = {
       version: 'v2',
       response_type: 'code',
       minimal_assurance_level: 'low',
-      auth_methods: process.env.SERVICE_PROFIEL_AUTH_METHODS || 'fas-citizen-bmid,fas-citizen-eid,fas-citizen-totp,fas-citizen-otp,aprofiel',
+      auth_methods: process.env.SERVICE_PROFIEL_AUTH_METHODS || 'fas-citizen-bmid,fas-citizen-eid,fas-citizen-totp,fas-citizen-otp,iam-aprofiel-userpass',
       client_id: process.env.SERVICE_PROFIEL_ACM_AUTH_CLIENT_ID || 'YOUR_CLIENT_ID',
       client_secret: process.env.SERVICE_PROFIEL_ACM_AUTH_CLIENT_SECRET || 'YOUR_CLIENT_SECRET',
       scope: 'astad.aprofiel.v1.username astad.aprofiel.v1.name astad.aprofiel.v1.avatar astad.aprofiel.v1.email astad.aprofiel.v1.phone crspersoon.givenName',
